da33cb88133a685fd3f210ca3ec1dd63d0df821f
lou <louamaya@me.com>
2020-08-10 23:32:04 -0700

    change root server

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 	session_start();
 
 	$timezone = date_default_timezone_set("America/Los_Angeles");
-	$con = mysqli_connect("localhost","root","","social");
-	// $con = mysqli_connect("162.241.24.185","apexhris_loam","8v9Xmnb!$1iaDc6Z","apexhris_amigo");
+	// $con = mysqli_connect("localhost","root","","social");
+	$con = mysqli_connect("162.241.24.185","apexhris_loam","8v9Xmnb!$1iaDc6Z","apexhris_amigo");
 	if (mysqli_connect_errno()){
 		echo "failed to myDB" .  mysqli_connect_errno();
 	}
